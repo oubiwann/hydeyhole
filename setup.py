165da80ae18d882ed597d014cb4f40a67d18b987
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2013-08-16 22:37:01 -0700

    Added setup file.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+from hydeyhole import meta
+from hydeyhole import util
+
+
+setup(
+    name=meta.display_name,
+    version=meta.version,
+    description=meta.description,
+    long_description=meta.long_description,
+    author=meta.author,
+    author_email=meta.author_email,
+    url=meta.url,
+    license=meta.license,
+    packages=find_packages() + ["twisted.plugins"],
+    package_data={
+        "twisted": ['plugins/hydeyhole.py']
+        },
+    install_requires=meta.requires,
+    zip_safe=False
+    )
+
+
+util.refresh_plugin_cache()
